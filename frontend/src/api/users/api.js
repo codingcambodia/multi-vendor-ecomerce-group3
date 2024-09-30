1dacb52e43671cb9b65b30e000a13889d1046140
codingcambodia <bbu.vannak.test@gmail.com>
2024-09-30 18:48:36 +0700

    fix url at api file

--- frontend/src/api/users/api.js
+++ frontend/src/api/users/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v2/user";
+const BASE_URL = "https://groupthree.shop/api/v2/user";
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
