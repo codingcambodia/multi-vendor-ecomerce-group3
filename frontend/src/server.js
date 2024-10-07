a50b6e000edefcc00e9b06e31dbcca2ec128fa06
codingcambodia <bbu.vannak.test@gmail.com>
2024-10-07 13:48:00 +0700

    switch backend url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-// export const server = "https://backend.groupthree.shop/api/v2";
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://backend.groupthree.shop/api/v2";
+// export const server = "http://localhost:8000/api/v2";
